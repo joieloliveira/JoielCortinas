27913fb8b8ea615fe8c9821d73b73563f47e8061
joieloliveira <59665976+joieloliveira@users.noreply.github.com>
2020-03-14 16:39:09 -0300

    add novo php cortinaFabrica
    cortinaFabrica POO junto com db_joielCortinas

---
+++ php/cortinaFabrica.php
@@ -0,0 +1,157 @@
+<?php
+
+class cortinaFabrica{
+	//variaveis da conexão 
+	public $servidor = "localhost";
+	public $usuario = "root";
+	public $senha = "";
+	public $dbname = "joielcortinas";
+	public $porta = 3308;
+	//variaveis das cortinas
+	public $cortina;//escolhido(a)
+	public $altura;//escolhido(a)
+	public $largura;//escolhido(a)
+	public $tecido;//escolhido(a)
+	public $forro;//escolhido(a)
+	public $roloBkTs;//escolhido(a)
+	public $lucro;//escolhido(a)
+	//public $conn = mysqli_connect($servidor, $usuario, $senha, $dbname, $porta);
+	function cortina($cortina){		
+		$this->cortina=$cortina;
+	}
+	
+	function medidas($altura,$largura){
+		$this->altura=$altura;
+		$this->largura=$largura;
+	}
+	
+	function tecido($tecido){		
+		$this->tecido=$tecido;
+	}
+	
+	function forro($forro){
+		$this->forro=$forro;
+	}
+	
+	function roloBkTs($roloBkTs){
+		$this->roloBkTs=$roloBkTs;
+	}
+	
+	function lucro($lucro){
+		$this->lucro=$lucro;
+	}
+	
+	function montaCortina(){
+		//cria conexão
+		$conn = mysqli_connect($this->servidor, $this->usuario, $this->senha, $this->dbname, $this->porta);
+		//Pega lucro no BD
+		$result_usuario_lucro= "SELECT lucro FROM usuarios where nome='joiel'";
+		$resultado_usuario_lucro = mysqli_query($conn, $result_usuario_lucro);
+		$row_usuario_lucro = mysqli_fetch_assoc($resultado_usuario_lucro); 
+		
+		// montaCortina Horizontal
+		if($this->cortina=="horizontal"){
+			$result_usuario_tecido = "SELECT valor FROM tecidos where nome='horizontal'";
+			$resultado_usuario_tecido = mysqli_query($conn, $result_usuario_tecido);
+			$row_usuario_tecido = mysqli_fetch_assoc($resultado_usuario_tecido);
+			return(($this->altura*$this->largura*$row_usuario_tecido['valor'])+200);
+		}
+		
+		// montaCortina vertical tecido
+		if($this->cortina=="verticalTecido"){
+			$result_usuario_tecido = "SELECT valor FROM tecidos where nome='verticalTecido'";
+			$resultado_usuario_tecido = mysqli_query($conn, $result_usuario_tecido);
+			$row_usuario_tecido = mysqli_fetch_assoc($resultado_usuario_tecido);
+			return(($this->altura*$this->largura*$row_usuario_tecido['valor'])+200);
+		}
+		
+		// montaCortina vertical pvc
+		if($this->cortina=="verticalPvc"){
+			$result_usuario_tecido = "SELECT valor FROM tecidos where nome='verticalPvc'";
+			$resultado_usuario_tecido = mysqli_query($conn, $result_usuario_tecido);
+			$row_usuario_tecido = mysqli_fetch_assoc($resultado_usuario_tecido);
+			return(($this->altura*$this->largura*$row_usuario_tecido['valor'])+200);
+		}
+		
+		// montaCortina rolô
+		if($this->cortina=="rolo"){
+			if($this->roloBkTs=="bk"){
+				$result_usuario_tecido = "SELECT valor FROM tecidos where nome='roloBk'";
+			}else if($this->roloBkTs=="tela"){
+				$result_usuario_tecido = "SELECT valor FROM tecidos where nome='roloTela'";
+			}	
+			$resultado_usuario_tecido = mysqli_query($conn, $result_usuario_tecido);
+			$row_usuario_tecido = mysqli_fetch_assoc($resultado_usuario_tecido);
+			return(($this->altura*$this->largura*$row_usuario_tecido['valor'])+250);
+		}
+		
+		// montaCortina ilhoes
+		if($this->cortina=="ilhoes"){
+			if($this->tecido=="voal"){
+				$result_usuario_tecido = "SELECT valor FROM tecidos where nome='voal'";
+			}else if($this->tecido=="linho"){
+				$result_usuario_tecido = "SELECT valor FROM tecidos where nome='linho'";
+			}
+			if($this->forro=="blackout"){
+				$result_usuario_forro = "SELECT valor FROM tecidos where nome='blackout'";
+			}else if($this->forro=="microfibra"){
+				$result_usuario_forro = "SELECT valor FROM tecidos where nome='microFibra'";
+			}
+			//pega tecido escolhido
+			$resultado_usuario_tecido = mysqli_query($conn, $result_usuario_tecido);
+			$row_usuario_tecido = mysqli_fetch_assoc($resultado_usuario_tecido);
+			//pega forro escolhido
+			$resultado_usuario_forro = mysqli_query($conn, $result_usuario_forro);
+			$row_usuario_forro = mysqli_fetch_assoc($resultado_usuario_forro);
+			//pega ilhoes na tabela peças
+			$result_usuario_ilhoes = "SELECT valor FROM pecas where nome='ilhoes'";
+			$resultado_usuario_ilhoes = mysqli_query($conn, $result_usuario_ilhoes);
+			$row_usuario_ilhoes = mysqli_fetch_assoc($resultado_usuario_ilhoes);
+			//pega entretela na tabela peças
+			$result_usuario_entretela = "SELECT valor FROM pecas where nome='entretela'";
+			$resultado_usuario_entretela = mysqli_query($conn, $result_usuario_entretela);
+			$row_usuario_entretela = mysqli_fetch_assoc($resultado_usuario_entretela);
+			//pega kite de montagem na tabela peças
+			if($this->largura<=1.5){$result_usuario_kite = "SELECT valor FROM pecas where nome='kite 1.5m'";}
+			else if($this->largura>1.5){$result_usuario_kite = "SELECT valor FROM pecas where nome='kite 2m'";}
+			$resultado_usuario_kite = mysqli_query($conn, $result_usuario_kite);
+			$row_usuario_kite = mysqli_fetch_assoc($resultado_usuario_kite);
+			//valor da cortina montada
+			return(($this->largura*3*$row_usuario_tecido['valor'])+($this->largura*3*$row_usuario_entretela['valor'])+($row_usuario_ilhoes['valor']*$this->largura*3*10)+($row_usuario_kite['valor'])+($row_usuario_forro['valor']*$this->largura*3)+200);
+		}
+		
+		// montaCortina americana
+		if($this->cortina=="americana"){
+			if($this->tecido=="voal"){
+				$result_usuario_tecido = "SELECT valor FROM tecidos where nome='voal'";
+			}else if($this->tecido=="linho"){
+				$result_usuario_tecido = "SELECT valor FROM tecidos where nome='linho'";
+			}
+			if($this->forro=="blackout"){
+				$result_usuario_forro = "SELECT valor FROM tecidos where nome='blackout'";
+			}else if($this->forro=="microfibra"){
+				$result_usuario_forro = "SELECT valor FROM tecidos where nome='microFibra'";
+			}
+			//pega tecido escolhido
+			$resultado_usuario_tecido = mysqli_query($conn, $result_usuario_tecido);
+			$row_usuario_tecido = mysqli_fetch_assoc($resultado_usuario_tecido);
+			//pega forro escolhido
+			$resultado_usuario_forro = mysqli_query($conn, $result_usuario_forro);
+			$row_usuario_forro = mysqli_fetch_assoc($resultado_usuario_forro);
+			//pega trilho na tabela peças
+			$result_usuario_trilho = "SELECT valor FROM pecas where nome='trilho'";
+			$resultado_usuario_trilho = mysqli_query($conn, $result_usuario_trilho);
+			$row_usuario_trilho = mysqli_fetch_assoc($resultado_usuario_trilho);
+			//pega entretela na tabela peças
+			$result_usuario_entretela = "SELECT valor FROM pecas where nome='entretela'";
+			$resultado_usuario_entretela = mysqli_query($conn, $result_usuario_entretela);
+			$row_usuario_entretela = mysqli_fetch_assoc($resultado_usuario_entretela);
+			//pega deslizantes na tabela peças
+			$result_usuario_deslizante = "SELECT valor FROM pecas where nome='deslizante'";
+			$resultado_usuario_deslizante = mysqli_query($conn, $result_usuario_deslizante);
+			$row_usuario_deslizante = mysqli_fetch_assoc($resultado_usuario_deslizante);
+			//valor da cortina montada
+			return(($this->largura*3*$row_usuario_tecido['valor'])+($this->largura*3*$row_usuario_entretela['valor'])+($row_usuario_deslizante['valor']*$this->largura*3*10)+($row_usuario_trilho['valor']*$this->largura)+($row_usuario_forro['valor']*$this->largura*3)+200);
+		}
+	}
+}
--- php/db_joielCortinas.php
+++ php/db_joielCortinas.php
@@ -1,102 +1,49 @@
 <?php
 
-$servidor = "localhost";
-$usuario = "root";
-$senha = "";
-$dbname = "joielcortinas";
-$porta = 3308;
-//Criar a conexao
-$conn = mysqli_connect($servidor, $usuario, $senha, $dbname, $porta);
-	
+require './cortinaFabrica.php';
+
 $altura = $_REQUEST["altura"];
 $largura = $_REQUEST["largura"];
 $cortina = $_REQUEST["cortina"];
 
-if($cortina=="ilhoes"||$cortina=="americana"){
-	$radioTecido = $_REQUEST["radioTecido"];
-	$radioBlackout = $_REQUEST["radioBlackout"];
-
-	//Pega o lucro por usuário
-	$result_usuario_lucro= "SELECT lucro FROM usuarios where nome='joiel'";
-	$resultado_usuario_lucro = mysqli_query($conn, $result_usuario_lucro);
-	$row_usuario_lucro = mysqli_fetch_assoc($resultado_usuario_lucro); 
-	//com blackout sem blackout
-	if($radioBlackout=="comBlackout"){
-		$result_usuario_blackout = "SELECT valor FROM tecidos where nome='blackout'";
-	}if($radioBlackout=="semBlackout"){
-		$result_usuario_blackout = "SELECT valor FROM tecidos where nome='microfibra'";
-	}
-	$resultado_usuario_blackout = mysqli_query($conn, $result_usuario_blackout);
-	$row_usuario_blackout = mysqli_fetch_assoc($resultado_usuario_blackout);
-	//pega o tecido escolhido
-	if($radioTecido=="voal"){
-		$result_usuario_tecido = "SELECT valor FROM tecidos where nome='voal'";
-	}if($radioTecido=="linho"){
-		$result_usuario_tecido = "SELECT valor FROM tecidos where nome='linho'";
-	}
-	$resultado_usuario_tecido = mysqli_query($conn, $result_usuario_tecido);
-	$row_usuario_tecido = mysqli_fetch_assoc($resultado_usuario_tecido);
-	//pega peças da cortina escolhida americana
-	if($cortina=="americana"){
-		$result_usuario_deslizante = "SELECT valor FROM pecas where nome='deslizante'";
-		$resultado_usuario_deslizante = mysqli_query($conn, $result_usuario_deslizante);
-		$row_usuario_deslizante = mysqli_fetch_assoc($resultado_usuario_deslizante);
-		
-		$result_usuario_entretela = "SELECT valor FROM pecas where nome='entretela'";
-		$resultado_usuario_entretela = mysqli_query($conn, $result_usuario_entretela);
-		$row_usuario_entretela = mysqli_fetch_assoc($resultado_usuario_entretela);
-		
-		$result_usuario_trilho = "SELECT valor FROM pecas where nome='trilho'";
-		$resultado_usuario_trilho = mysqli_query($conn, $result_usuario_trilho);
-		$row_usuario_trilho = mysqli_fetch_assoc($resultado_usuario_trilho);
-		
-		echo (($largura*3*$row_usuario_tecido['valor'])+($largura*3*$row_usuario_entretela['valor'])+($row_usuario_deslizante['valor']*$largura*3*10)+($row_usuario_trilho['valor']*$largura)+($row_usuario_blackout['valor']*$largura*3)+$row_usuario_lucro['lucro']);
-	//pega peças da cortina escolhida ilhoes	
-	}if($cortina=="ilhoes"){
-		$result_usuario_ilhoes = "SELECT valor FROM pecas where nome='ilhoes'";
-		$resultado_usuario_ilhoes = mysqli_query($conn, $result_usuario_ilhoes);
-		$row_usuario_ilhoes = mysqli_fetch_assoc($resultado_usuario_ilhoes);
-		
-		$result_usuario_entretela = "SELECT valor FROM pecas where nome='entretela'";
-		$resultado_usuario_entretela = mysqli_query($conn, $result_usuario_entretela);
-		$row_usuario_entretela = mysqli_fetch_assoc($resultado_usuario_entretela);
-		
-		if($largura<=1.5){$result_usuario_kite = "SELECT valor FROM pecas where nome='kite 1.5m'";}
-		if($largura>1.5){$result_usuario_kite = "SELECT valor FROM pecas where nome='kite 2m'";}
-		$resultado_usuario_kite = mysqli_query($conn, $result_usuario_kite);
-		$row_usuario_kite = mysqli_fetch_assoc($resultado_usuario_kite);
-		
-		echo (($largura*3*$row_usuario_tecido['valor'])+($largura*3*$row_usuario_entretela['valor'])+($row_usuario_ilhoes['valor']*$largura*3*10)+($row_usuario_kite['valor'])+($row_usuario_blackout['valor']*$largura*3)+$row_usuario_lucro['lucro']);	
-	}
-}
-
-
 if($cortina=="horizontal"){
-	$result_usuario_tecido = "SELECT valor FROM tecidos where nome='horizontal'";
-	$resultado_usuario_tecido = mysqli_query($conn, $result_usuario_tecido);
-	$row_usuario_tecido = mysqli_fetch_assoc($resultado_usuario_tecido);
-	echo(($altura*$largura*$row_usuario_tecido['valor'])+150);
-		
-}if($cortina=="verticalTecido"){
-	$result_usuario_tecido = "SELECT valor FROM tecidos where nome='verticalTecido'";
-	$resultado_usuario_tecido = mysqli_query($conn, $result_usuario_tecido);
-	$row_usuario_tecido = mysqli_fetch_assoc($resultado_usuario_tecido);
-	echo(($altura*$largura*$row_usuario_tecido['valor'])+150);
-		
-}if($cortina=="verticalPvc"){
-	$result_usuario_tecido = "SELECT valor FROM tecidos where nome='verticalPvc'";
-	$resultado_usuario_tecido = mysqli_query($conn, $result_usuario_tecido);
-	$row_usuario_tecido = mysqli_fetch_assoc($resultado_usuario_tecido);
-	echo(($altura*$largura*$row_usuario_tecido['valor'])+150);
-		
-}if($cortina=="rolo"){
-	$radioRolo = $_REQUEST["radioRolo"];
-	if($radioRolo=="bk"){
-		$result_usuario_tecido = "SELECT valor FROM tecidos where nome='roloBk'";
-	}else if($radioRolo=="tela"){
-		$result_usuario_tecido = "SELECT valor FROM tecidos where nome='roloTela'";
-	}	
-	$resultado_usuario_tecido = mysqli_query($conn, $result_usuario_tecido);
-	$row_usuario_tecido = mysqli_fetch_assoc($resultado_usuario_tecido);
-	echo(($altura*$largura*$row_usuario_tecido['valor'])+200);		
+	$horizontal = new cortinaFabrica();
+	$horizontal->cortina($cortina);
+	$horizontal->medidas($largura,$altura);
+	echo $horizontal->montaCortina();
+}else if($cortina=="verticalTecido"){
+	$verticalTecido = new cortinaFabrica();
+	$verticalTecido->cortina($cortina);
+	$verticalTecido->medidas($largura,$altura);
+	echo $verticalTecido->montaCortina();
+}else if($cortina=="verticalPvc"){
+	$verticalPvc = new cortinaFabrica();
+	$verticalPvc->cortina($cortina);
+	$verticalPvc->medidas($largura,$altura);
+	echo $verticalPvc->montaCortina();
+}else if($cortina=="rolo"){
+	$roloBkTs = $_REQUEST["radioRolo"];
+	$rolo = new cortinaFabrica();
+	$rolo->cortina($cortina);
+	$rolo->medidas($largura,$altura);
+	$rolo->roloBkTs($roloBkTs);
+	echo $rolo->montaCortina();
+}else if($cortina=="ilhoes"){
+	$tecido = $_REQUEST["radioTecido"];
+	$forro = $_REQUEST["radioBlackout"];
+	$ilhoes = new cortinaFabrica();
+	$ilhoes->cortina($cortina);
+	$ilhoes->medidas($largura,$altura);
+	$ilhoes->tecido($tecido);
+	$ilhoes->forro($forro);
+	echo $ilhoes->montaCortina();
+}else if($cortina=="americana"){
+	$tecido = $_REQUEST["radioTecido"];
+	$forro = $_REQUEST["radioBlackout"];
+	$americana = new cortinaFabrica();
+	$americana->cortina($cortina);
+	$americana->medidas($largura,$altura);
+	$americana->tecido($tecido);
+	$americana->forro($forro);
+	echo $americana->montaCortina();
 }
